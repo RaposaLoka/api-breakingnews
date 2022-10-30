74135c3fd272fa077811b84a87c35e123aa2e9ce
Yuri Ichitani <yurirj07@gmail.com>
2022-10-30 09:21:53 +0900

    a

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const connectDatabase = require("./src/database/db");
 
 const userRoute = require("./src/routes/user.route");
 
-const port = 3000;
+const port = ("port", process.env.PORT || 3000);
 
 connectDatabase();
 app.use(express.json());
--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,5 +1,4 @@
 const userService = require("../services/user.service");
-const mongoose = require("mongoose");
 
 const create = async (req, res) => {
   const { name, username, email, password, avatar, background } = req.body;
@@ -38,38 +37,17 @@ const findAll = async (req, res) => {
 };
 
 const findById = async (req, res) => {
-  const id = req.params.id;
-
-  if (!mongoose.Types.ObjectId.isValid(id)) {
-    return res.status(400).send({ message: "Invalid ID" });
-  }
-
-  const user = await userService.findByIdService(id);
-
-  if (!user) {
-    return res.status(400).send({ message: "User not found" });
-  }
-
+  const user = req.userResp;
   res.send(user);
 };
 
 const update = async (req, res) => {
-  const id = req.params.id;
+  const id = req.id;
 
-  if (!mongoose.Types.ObjectId.isValid(id)) {
-    return res.status(400).send({ message: "Invalid ID" });
-  }
-  
   const { name, username, email, password, avatar, background } = req.body;
 
   if (!name && !username && !email && !password && !avatar && !background) {
     res.status(400).send({ message: "Submit at least one field for update" });
-  }
-
-  const user = await userService.findByIdService(id);
-
-  if (!user) {
-    return res.status(400).send({ message: "User not found" });
   }
 
   await userService.updateService(
