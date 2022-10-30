8553d7ad352ddadf66885fd5d67d179aee4d7439
RaposaLoka <96669227+RaposaLoka@users.noreply.github.com>
2022-10-30 09:24:53 +0900

    Update user.route.js

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -2,8 +2,5 @@ const route = require("express").Router();
 const userController = require("../controllers/user.controller");
 
 route.post("/", userController.create);
-route.get("/", (req, res) => { 
-  res.send({ message: "teste"});}
-
 
 module.exports = route;
